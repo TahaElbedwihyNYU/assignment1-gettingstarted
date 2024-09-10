d595b088bd65f5da6be817486a015b6a8128201e
TahaElbedwihyNYU <te2215@nyu.edu>
2024-09-10 19:24:18 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -6,7 +6,7 @@ def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
     if question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "5"
+        answer = "4"
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
@@ -23,7 +23,7 @@ def welcome_assignment_answers(question):
         answer = "No"
     
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = "1"
+        answer = "5"
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
