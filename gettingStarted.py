f380020a2a6c80af883f1dd7698135c94e46fce7
PayInCash <te2215@nyu.edu>
2024-09-10 19:10:20 -0400

    test

--- gettingStarted.py
+++ gettingStarted.py
@@ -5,24 +5,25 @@
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
+    if question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = "1"
+    elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "pcap"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "Yes"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
         answer = "3e0c5e3abc6a76d819775d460224921b92295e1132d29f170bae42a866f9b57c"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
-    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "3"
+    
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = "5"
+        answer = "2"
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
